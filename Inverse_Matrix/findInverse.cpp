4bfd51f76a406143dfed6b32b134ebbff0eb176c
Anthony Barranco <anthony@lssclan.org>
2013-12-22 23:24:44 -0500

    Inverse_Matrix Found Cofactor
    Created a function that can find the minors and make a cofactor matrix.

--- Inverse_Matrix/findInverse.cpp
+++ Inverse_Matrix/findInverse.cpp
@@ -33,12 +33,55 @@ void takeInput(Matrix &m)
     }
 }
 
+//Take matrixs a matrix, finds the minor, and makes cofactor.
+//Returns the individual number that belongs for x and y.
+int findCofactor(Matrix m, int x, int y)
+{
+    int cofactor;
+    cofactor = m( ((x+1) % 3),((y+1) % 3))
+    * m( ((x+2) % 3) , ((y+2) % 3) )
+    - m( ((x+1) % 3), ((y+2) % 3))
+    * m( ((x+2) % 3) , ((y+1) %3) );
+
+    return cofactor;
+}
+
+Matrix createCofactor(Matrix m)
+{
+    Matrix dx(m.dx,m.dy);
+    for(int x = 0; x < m.dx; x++)
+    {
+        for(int y = 0; y < m.dy; y++)
+        {
+            dx(x,y) = findCofactor(m,x,y);
+        }
+    }
+    return dx;
+}
+
 
 int main()
 {
-
     Matrix x(3,3);
-    takeInput(x);
-    cout << "Matrix x\n" << x;
+    x(0,0) = -1;
+    x(0,1) = -2;
+    x(0,2) = 2;
+
+    x(1,0) = 2;
+    x(1,1) = 1;
+    x(1,2) = 1;
+
+    x(2,0) = 3;
+    x(2,1) = 4;
+    x(2,2) = 5;
+    //takeInput(x); //Automated input
+    cout << "Matrix x\n" << x << endl;
+
+    //STEP 1 & 2 Find the minors and Cofactor matrix.
+    Matrix coFactor = createCofactor(x);
+    cout << "CoFactor\n" << coFactor << endl;
+
+    //STEP 3 Find determinant
+
     return 0;
 }
